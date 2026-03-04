ed6612f902fcda245b4d026dccb7b5fc955166cd
muadhbelkacem <muadhbelkacem@gmail.com>
2026-03-04 20:28:30 +0100

    Add qtile topbar

--- .config/qtile/modules/screens.py
+++ .config/qtile/modules/screens.py
@@ -1,5 +1,15 @@
 from libqtile.config import Screen
+from libqtile import bar
+from .widgets import widgets
+from .colors import background_color
 
 screens = [
-    Screen(),
+    Screen(
+        top=bar.Bar(
+            widgets,
+            24,
+            background=background_color,
+            margin=[5, 5, 0, 5],
+        ),
+    ),
 ]
