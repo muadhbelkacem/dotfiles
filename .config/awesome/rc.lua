87fb43b5b8560fe32e5dc478fb0e1f90bc9c6b8c
muadhbelkacem <muadhbelkacem@gmail.com>
2026-03-05 14:38:28 +0100

    Add awesome tile bottom layout

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -49,9 +49,7 @@ modkey = "Mod4"
 awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.floating,
-    --awful.layout.suit.tile.left,
-    --awful.layout.suit.tile.bottom,
-    --awful.layout.suit.max,
+    awful.layout.suit.tile.bottom,
 }
 -- }}}
 
