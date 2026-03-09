361bfd6cf228f4a63b17398e14f9a75cffa7b297
muadhbelkacem <muadhbelkacem@gmail.com>
2026-03-09 04:33:48 +0100

    Update emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,13 @@
+;; Disable UI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Show line numbers
+(global-display-line-numbers-mode 1)
+
+;; Disable startup screen
+(setq inhibit-startup-screen t)
+
+;; Enable column number
+(column-number-mode)
