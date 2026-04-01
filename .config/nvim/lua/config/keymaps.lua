a2ee924884c4a49b2e382061d968448924514c12
muadhbelkacem <muadhbelkacem@gmail.com>
2026-04-01 01:22:46 +0100

    Nvim map :w to ctrl + s

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "U", "<C-r>")
 
 -- Save and quit current file quicker
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { silent = false })
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { silent = false })
 
 -- Close currently active buffer
